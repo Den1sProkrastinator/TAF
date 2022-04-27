20653ce78b1394c1468cdc447be0feba3e995915
Den1sprokrastinator <puteec228@gmail.com>
2022-04-27 19:08:12 +0300

    Snova

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import pages.LoginPage;
+import Pages.LoginPage;
 import services.BrowsersService;
 
 public class LoginTest {
