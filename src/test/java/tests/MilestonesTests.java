764370a02c1091ffdbd799a9dd1690c81c5d700e
Den1sprokrastinator <puteec228@gmail.com>
2022-06-02 02:07:56 +0300

    Allure HW

--- src/test/java/tests/MilestonesTests.java
+++ src/test/java/tests/MilestonesTests.java
@@ -2,15 +2,23 @@ package tests;
 
 import baseEntities.BaseTest;
 import configuration.ReadProperties;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
 import models.MilestoneBuilder;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class MilestonesTests extends BaseTest {
 
 
-    @Test(priority = 1)
-    public void addMilestones() {
+    @Step("Milestone name  - Denis ")
+    @Test(priority = 1 , description = "Add new Milestone")
+    public void addMilestones() throws IOException {
         MilestoneBuilder milestone = new MilestoneBuilder.Builder()
                 .withName("Denis")
                 .withReference("reference")
@@ -23,18 +31,19 @@ public class MilestonesTests extends BaseTest {
         milestonesStep.addMilestones();
         Assert.assertTrue(createAndEditMilestoneStep.createMilestones
                 (milestone).isPageOpened());
+        getBytes("strelkov.jpeg");
     }
 
-
-    @Test(priority = 2)
+    @Step("Milestone your are reading ")
+    @Test(priority = 2 , description = "Read milestone")
     public void readMilestone() {
 
         loginAndOpenMilestoneMenu();
         Assert.assertTrue(milestonesStep.readMilestones("Denis").isPageOpened());
 
     }
-
-    @Test(priority = 3)
+    @Step("Milestone your are starting ")
+    @Test(priority = 3, description = "Start milestone")
     public void startMilestone() {
         loginAndOpenMilestoneMenu();
         milestonesStep.readMilestones("Denis");
@@ -43,8 +52,8 @@ public class MilestonesTests extends BaseTest {
                 .getSuccessAction().getText(), "Successfully started the milestone.");
     }
 
-
-    @Test(priority = 4)
+    @Step("milestone to be removed ")
+    @Test(priority = 4, description = "Edit milestone")
     public void editMilestone() {
         loginAndOpenMilestoneMenu();
 
@@ -54,7 +63,7 @@ public class MilestonesTests extends BaseTest {
                 .getSuccessAction().getText(), "Successfully updated the milestone.");
     }
 
-    @Test(priority = 5)
+    @Test(priority = 5, description = "Delete milestone")
     public void deleteMilestone() {
         loginAndOpenMilestoneMenu();
 
@@ -69,6 +78,11 @@ public class MilestonesTests extends BaseTest {
         dashBoardStep.projectSelection("Denis_QA18_PO_HW");
         projectsStep.menuNavigation("Milestones");
 
+    }
+
+    @Attachment
+    public static byte[] getBytes(String resourceName) throws IOException {
+        return Files.readAllBytes(Paths.get("src/test/resources", resourceName));
     }
 
 
