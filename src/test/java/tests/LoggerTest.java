c0ad355ac102fc39eedd3c30f1cb99f4754593ce
Den1sprokrastinator <puteec228@gmail.com>
2022-06-06 17:21:00 +0300

    HW

---
+++ src/test/java/tests/LoggerTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Test;
+
+public class LoggerTest {
+
+    private static final Logger logger = LogManager.getLogger(LoggerTest.class);
+
+    @Test
+    public void logLevelsTest(){
+        logger.trace("Trace Message : test start");
+        logger.debug("Debug Message...");
+        logger.info("Info Message ...");
+        logger.warn("Warning Message ...");
+        logger.error("Error Message...");
+        logger.fatal("Fatal Message : test end");
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
