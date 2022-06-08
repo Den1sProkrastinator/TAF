4133f4529e16b13098e21f5f72e9ab8f7ab3829b
Den1sprokrastinator <puteec228@gmail.com>
2022-06-08 21:54:46 +0300

    HW

--- src/test/java/tests/api/TestRailApiTest.java
+++ src/test/java/tests/api/TestRailApiTest.java
@@ -7,48 +7,91 @@ import io.restassured.http.ContentType;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import models.Project;
+import models.ProjectType;
 import org.apache.http.HttpStatus;
 import org.apache.http.protocol.HTTP;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
-public class TestRailApiTest extends BaseApitTest{
+public class TestRailApiTest extends BaseApitTest {
 
-@Test
-    public void getAllUserTest(){
-    // Setup request Object
-    RequestSpecification httpRequest = given();
-    httpRequest.header(HTTP.CONTENT_TYPE, ContentType.JSON);
-    httpRequest.auth().preemptive().basic(ReadProperties.username(), ReadProperties.password());
+    @Test
+    public void getAllUserTest() {
+        // Setup request Object
+        RequestSpecification httpRequest = given();
+        httpRequest.header(HTTP.CONTENT_TYPE, ContentType.JSON);
+        httpRequest.auth().preemptive().basic(ReadProperties.username(), ReadProperties.password());
 
-    // Setup Response Object
-    URI endpoint;
-    Response response = httpRequest.request(Method.GET, Endpoints.GET_PROJECTS);
+        // Setup Response Object
+        URI endpoint;
+        Response response = httpRequest.request(Method.GET, Endpoints.GET_PROJECTS);
 
-    // Get Response Status
-    int statusCode = response.getStatusCode();
-    System.out.println("Status Code: " + statusCode);
-    Assert.assertEquals(statusCode, HttpStatus.SC_OK);
+        // Get Response Status
+        int statusCode = response.getStatusCode();
+        System.out.println("Status Code: " + statusCode);
+        Assert.assertEquals(statusCode, HttpStatus.SC_OK);
 
-    // Get Response Body
-    String responseBody = response.getBody().asString();
-    System.out.println("Response: " + responseBody);
-}
+        // Get Response Body
+        String responseBody = response.getBody().asString();
+        System.out.println("Response: " + responseBody);
+    }
 
-@Test
-    public void test (){
-    given()
+    @Test
+    public void getAPIShortTest() {
+        given()
 //            .header(HTTP.CONTENT_TYPE,ContentType.JSON)
 //            .auth().preemptive().basic(ReadProperties.username(),ReadProperties.password())
-            .when()
-            .get(Endpoints.GET_PROJECTS)
-            .then()
-            .log().status()
-            .log().body()
-            .statusCode(HttpStatus.SC_OK);
-}
+                .when()
+                .get(Endpoints.GET_PROJECTS)
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void addProjectTest(){
+        Project newProject = Project.builder()
+                .name("WP_Test_01")
+                .build();
+
+        given()
+                .body(String.format("{\n" +
+                        "  \"name\": \"%s\"\n" +
+                        "}", newProject.getName()))
+                .when()
+                .post(Endpoints.ADD_PROJECT)
+                .then()
+                .log().body()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void addProject2() {
+        Project project = Project.builder()
+                .name("WP_Project_02")
+                .typeOfProject(ProjectType.SINGLE_SUITE_MODE)
+                .build();
+
+        Map<String, Object> jsonAsMap = new HashMap<>();
+        jsonAsMap.put("name", project.getName());
+        jsonAsMap.put("suite_mode", project.getTypeOfProject());
+
+        given()
+                .body(jsonAsMap)
+                .when()
+                .post(Endpoints.ADD_PROJECT)
+                .then()
+                .log().body()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+
 }
