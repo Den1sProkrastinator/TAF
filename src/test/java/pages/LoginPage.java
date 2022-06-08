779900744671ad26bcf86bda1b3e47a5278cf136
Den1sprokrastinator <puteec228@gmail.com>
2022-06-08 16:50:16 +0300

    HW

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -8,11 +8,39 @@ import tests.SelenideTest;
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
+
+
+
+    //locators
     public SelenideElement email = $("[type='text']");
 
     public SelenideElement  psw =$("[type='password']");
     public SelenideElement button= $(".button.is-vcentered");
 
     public SelenideElement alertText=$(".notification.is-danger");
+
+
+    //corpuscular methods
+
+    public SelenideElement getEmail() {
+        return email;
+    }
+
+    public SelenideElement getPsw() {
+        return psw;
+    }
+
+    public SelenideElement getButton() {
+        return button;
+    }
+
+    public SelenideElement getAlertText() {
+        return alertText;
+    }
+
+    public LoginPage successLogin(){
+
+    }
+
 
 }
