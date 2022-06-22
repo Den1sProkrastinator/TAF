1ce42ea26a6254574ca143c0cc302c13748d3a74
Den1sprokrastinator <puteec228@gmail.com>
2022-06-22 20:07:55 +0300

    Lesson

--- src/test/java/tests/SelenideTest.java
+++ src/test/java/tests/SelenideTest.java
@@ -54,8 +54,7 @@ public class SelenideTest {
         loginPage.psw.setValue(ReadProperties.password());
         loginPage.button.click();
 
-        $(".page_title").shouldBe(visible).shouldHave(text("All projects"));
-
+        $(".page_title").shouldBe(visible).shouldHave(text("All project1"));
 
     }
 
